c6c90fc5af3df302c61b4aa6f81493f994c0eb6f
astronaut125 <astronaut@cruzantonio.net>
2025-07-16 17:30:44 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+// idk tbh what is happening but we are learning
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+
+const userRouter = require("./routes/users");
+app.use("/users", userRouter);
+
+app.listen(3000);
